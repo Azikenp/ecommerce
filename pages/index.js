60c9a6583cbeb247bea2d8a330bb147c523d2d21
Azikenp <azikenp26@gmail.com>
2025-01-12 17:34:14 +0100

    typo

--- pages/index.js
+++ pages/index.js
@@ -4,14 +4,14 @@ const Home = () => {
   return (
     <>
       HeroBanner
-      
+
       <div>
         <h2>Best Selling Products</h2>
         <p>get Speakers of many variations</p>
       </div>
       <div>{["Product1", "Product2"].map((product) => product)}</div>
 
-      Footer
+      Footers
     </>
   );
 };
