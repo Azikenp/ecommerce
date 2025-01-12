a2979fc2aa0d9e586adda161a7c05b91ef942b12
Azikenp <azikenp26@gmail.com>
2025-01-12 17:42:41 +0100

    styles tested

--- pages/index.js
+++ pages/index.js
@@ -4,13 +4,13 @@ const Home = () => {
   return (
     <>
       HeroBanner
-      
-      <div>
+      <div className="products-heading">
         <h2>Best Selling Products</h2>
         <p>get Speakers of many variations</p>
       </div>
-      <div>{["Product1", "Product2"].map((product) => product)}</div>
-
+      <div className="products-container">
+        {["Product1", "Product2"].map((product) => product)}
+      </div>
       Footer
     </>
   );
