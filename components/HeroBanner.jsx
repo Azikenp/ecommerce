907bc817b80a9b6ff15ad3e8c2a2ecb0f793afd7
Azikenp <azikenp26@gmail.com>
2025-01-13 23:07:46 +0100

    banner done

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,22 +1,27 @@
+import { urlFor } from "@/lib/client";
 import Link from "next/link";
 import React from "react";
 
-const HeroBanner = ({heroBanner}) => {
+const HeroBanner = ({ heroBanner }) => {
   return (
     <div className="hero-banner-container">
       <div>
         <p className="beats-solo">{heroBanner.smallText}</p>
         <h3>{heroBanner.midText}</h3>
         <h1>{heroBanner.largeText1}</h1>
-        <img src="" alt="headpohnes" className="hero-banner-image" />
+        <img
+          src={urlFor(heroBanner.image)}
+          alt="headpohnes"
+          className="hero-banner-image"
+        />
 
         <div>
-          <Link href="/products/ID">
-            <button type="button">BUTTON TEXT</button>
+          <Link href={`/product/${heroBanner.product}`}>
+            <button type="button">{heroBanner.buttonText}</button>
           </Link>
           <div className="desc">
-            <h5>Description</h5>
-            <p>DESCRIPTION</p>
+            {/* <h5>Description</h5> */}
+            <p>{heroBanner.desc}</p>
           </div>
         </div>
       </div>
