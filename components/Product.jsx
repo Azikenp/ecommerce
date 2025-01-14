cd35ee8770428ec23171202b43f18793c4658b11
Azikenp <azikenp26@gmail.com>
2025-01-14 21:31:45 +0100

    dupe

--- components/Product.jsx
+++ components/Product.jsx
@@ -17,6 +17,7 @@ const Product = ({ product: { image, name, slug, price } }) => {
           />
           <p className="product-name">{name}</p>
           <p className="product-price">${price}</p>
+          <p className="product-price">${price}</p>
         </div>
       </Link>
     </div>
