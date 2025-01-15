cd9c63996df4421187f3c0e492730f4fad82fd43
Azikenp <azikenp26@gmail.com>
2025-01-15 22:40:17 +0100

    get static props for product details

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,9 +1,29 @@
-import React from 'react'
+import { urlFor } from "@/lib/client";
+import React from "react";
 
 const ProductDetails = () => {
   return (
-    <div>ProductDetails</div>
-  )
-}
+    <div className="product-detail-container">
+      <div>
+        <div className="image-container">
+          <img src="" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default ProductDetails
+export const getStaticProps = async ({ params: { slug } }) => {
+  const query = `*[_type == "product" & slug.current == '${slug}][0]`;
+  const productsQuery  = '*[_type == "product"]'
+
+  const product = await client.fetch(query);
+  const products = await client.fetch(productsQuery);
+
+
+  return {
+    props: { product, products },
+  };
+};
+
+export default ProductDetails;
