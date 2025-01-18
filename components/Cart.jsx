3a85f8e420910ec1aa235038ed7a331b93e75245
Azikenp <azikenp26@gmail.com>
2025-01-18 22:26:55 +0100

    cart logic

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import { useStateContext } from "@/context/StateContext";
+import { urlFor } from "@/lib/client";
+import Link from "next/link";
+import { useRef } from "react";
+import toast from "react-hot-toast";
+import {
+  AiOutlineMinus,
+  AiOutlinePlus,
+  AiOutlineLeft,
+  AiOutlineShopping,
+} from "react-icons/ai";
+import { TiDeleteOutline } from "react-icons/ti";
 
 const Cart = () => {
-  return (
-    <div>Cart</div>
-  )
-}
+  const cartRef = useRef();
+  const { totalQuantities, totalPrice, cartItems, setShowCart } =
+    useStateContext();
 
-export default Cart
+  return (
+    <div className="cart-wrapper" ref={cartRef}>
+      <div className="cart-container">
+        <button
+          type="button"
+          className="cart-heading"
+          onClick={() => setShowCart(false)}
+        >
+          <AiOutlineLeft />
+          <span className="heading">Your Cart</span>
+          <span className="cart-num-items">({totalQuantities} items)</span>
+        </button>
+
+        {cartItems.length < 1 && (
+          <div className="empty-cart">
+            <AiOutlineShopping size={150} />
+            <h3>Your shopping bag is empty</h3>
+            <Link href="/">
+              <button
+                type="button"
+                className="btn"
+                onClick={() => setShowCart(false)}
+              >
+                Continue shopping
+              </button>
+            </Link>
+          </div>
+        )}
+
+        <div>
+          {cartItems.length >= 1 &&
+            cartItems.map((item) => (
+              <div className="product" key={item._id}>
+                <img
+                  src={urlFor(item?.image[0])}
+                  className="cart-product-image"
+                />
+              </div>
+            ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
