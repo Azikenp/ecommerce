5f44b3ebfb75da8269cabfa43c62c7f6abcc753e
Azikenp <azikenp26@gmail.com>
2025-01-18 21:10:13 +0100

    selecte image on hover

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,5 +1,6 @@
 import { Product } from "@/components";
 import { client, urlFor } from "@/lib/client";
+import { useState } from "react";
 import {
   AiFillStar,
   AiOutlineMinus,
@@ -9,18 +10,26 @@ import {
 
 const ProductDetails = ({ products, product }) => {
   const { image, name, details, price } = product;
+  const [index, setIndex] = useState(0);
+
   return (
     <div>
       <div className="product-detail-container">
         <div>
           <div className="image-container">
-            <img src={urlFor(image && image[0])} />
+            <img src={urlFor(image && image[index])} className="product-detail-image" />
           </div>
-          {/* <div className="small-images-container">
-          {image?.map((item, index) => (
-            <img src={urlFor(item)} className="" onMouseEnter={} />
-          ))}
-        </div> */}
+          <div className="small-images-container">
+            {image?.map((item, i) => (
+              <img
+                src={urlFor(item)}
+                className={
+                  i === index ? "small-image selected-image" : "small-image"
+                }
+                onMouseEnter={() => setIndex(i)}
+              />
+            ))}
+          </div>
         </div>
 
         <div className="product-detail-desc">
