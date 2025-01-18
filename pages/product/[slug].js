7cd741c7a9c0c300da26028fc1214a047099f78a
Azikenp <azikenp26@gmail.com>
2025-01-18 21:01:08 +0100

    marwue style

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -67,9 +67,9 @@ const ProductDetails = ({ products, product }) => {
       <div className="maylike-products-wrapper">
         <h2>You may also like</h2>
         <div className="marquee">
-          <div className="maylike-products-container">
+          <div className="maylike-products-container track">
             {products.map((item) => (
-              <Product />
+              <Product key={item._id} product={item} />
             ))}
           </div>
         </div>
