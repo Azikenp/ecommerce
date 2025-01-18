668284680865e9dc4c73b2d747e26a005992f3e7
Azikenp <azikenp26@gmail.com>
2025-01-18 21:21:27 +0100

    context logic done

---
+++ context/StateContext.js
@@ -0,0 +1,20 @@
+import React, { createContext, useContext, useEffect, useState } from "react";
+import { toast } from "react-hot-toast";
+
+const Context = createContext();
+
+export const StateContext = ({ children }) => {
+  const [showCart, setShowCart] = useState(false);
+  const [cartItems, setCartItems] = useState();
+  const [totalPrice, setTotalPrice] = useState();
+  const [totalQuatities, setTotalQuatities] = useState();
+  const [qty, setQty] = useState(1);
+};
+
+return (
+  <ContextProvider
+    value={{ showCart, cartItems, totalPrice, totalQuatities, qty }}
+  >
+    {children}
+  </ContextProvider>
+);
