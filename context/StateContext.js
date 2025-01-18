aec344e3b17cdc9f642e407ef41b41f83f99ee59
Azikenp <azikenp26@gmail.com>
2025-01-18 21:32:40 +0100

    first context funs

--- context/StateContext.js
+++ context/StateContext.js
@@ -10,9 +10,20 @@ export const StateContext = ({ children }) => {
   const [totalQuatities, setTotalQuatities] = useState();
   const [qty, setQty] = useState(1);
 
+  const incQty = () => {
+    setQty((prevQty) => prevQty + 1);
+  };
+
+  const decQty = () => {
+    setQty((prevQty) => {
+      if (prevQty - 1 < 1) return 1;
+      return prevQty - 1;
+    });
+  };
+
   return (
     <Context.Provider
-      value={{ showCart, cartItems, totalPrice, totalQuatities, qty }}
+      value={{ showCart, cartItems, totalPrice, totalQuatities, qty ,incQty, decQty }}
     >
       {children}
     </Context.Provider>
