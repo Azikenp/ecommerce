b7bcb474e0decc9039ca0fe29eaf6b188832c544
Azikenp <azikenp26@gmail.com>
2025-01-19 14:01:48 +0100

    addon func

--- context/StateContext.js
+++ context/StateContext.js
@@ -14,6 +14,7 @@ export const StateContext = ({ children }) => {
   let index;
 
   const onAdd = (product, quantity) => {
+    
     const checkProductInCart = cartItems.find(
       (item) => item._id === product._id
     );
