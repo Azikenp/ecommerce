31f06f17ef7dc4e2a9cedff06d17edc0f35daa4e
Azikenp <azikenp26@gmail.com>
2025-01-21 23:34:37 +0100

    stripe

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
   if (req.method === "POST") {
     try {
     } catch (error) {
-      res.status(500).json({ statusCode: 500, message: error.message });
+      res.status(500).json({ statusCode: 500, message: error.messages });
     }
   }
 }
