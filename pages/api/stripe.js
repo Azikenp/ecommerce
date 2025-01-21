b6e96edc3031a329d0404c65e9cc7e1471900060
Azikenp <azikenp26@gmail.com>
2025-01-22 00:12:53 +0100

    additional params for stripe

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -4,6 +4,10 @@ export default async function handler(req, res) {
   if (req.method === "POST") {
     try {
       const params = {
+        submit_type: pay,
+        mode: payment,
+        payment_method_types: ["card"],
+        billing_address_collection: "auto",
         line_items: [
           {
             // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
