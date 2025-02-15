8e0925805904908aefb098d713a3b9420b83f8a8
Azikenp <azikenp26@gmail.com>
2025-02-15 15:22:31 +0100

    cart

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -37,6 +37,7 @@ const Cart = () => {
     if (response.statusCode === 500) return;
 
     const data = await response.json();
+    
     toast.loading("Redirecting...");
 
     stripe.redirectToCheckout({ sessionId: data.id });
